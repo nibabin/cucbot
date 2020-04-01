eb7167cc6e0a99e1a5c454d2cb542b71156b3fb9
Claudiu Babin <nibabin@Claudius-Mac-mini.local>
2020-04-01 16:22:42 +0300

    added timer and some tiny features

--- index.js
+++ index.js
@@ -20,23 +20,38 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 	const number = Number(args.shift());
-	const intrebare = pachet[number - 1];
-	if(command === 'q'){
-		if(Math.floor(number) != number || number < 0 || number >= pachet.length) message.channel.send("Invalid");
-		else{
-			const time = Number(args.shift());
-			message.channel.send(delimitator_a + intrebare.toString().split(delimitator_b)[0]);
-			/*
-			const channel1 = client.channels.find('name', 'echipa-roz')
-			setTimeout(function(){
-				channel1.send("10 secunde");
-			}, (time - 10) * 1000);
-			*/
+	if(Math.floor(number) != number || number < 0 || number >= pachet.length) message.channel.send("Invalid");
+	else{
+		const intrebare = pachet[number - 1];
+		const text = intrebare.toString().split(delimitator_b)[0];
+		if(text.includes("Material distributiv") || text.includes("material distributiv") || text.includes("imagine")){
+			message.channel.send("NU! Contine material distributiv.")
 		}
-	}
-	else if(command === 'a'){
-		if(Math.floor(number) != number || number < 0 || number >= pachet.length) message.channel.send("Invalid");
-		else message.channel.send(delimitator_b + intrebare.toString().split(delimitator_b)[1]);
+		else{
+			if(command === 'q'){
+				message.channel.send(delimitator_a + intrebare.toString().split(delimitator_b)[0]);
+			}
+			else if(command === 'a'){
+				message.channel.send(delimitator_b + intrebare.toString().split(delimitator_b)[1]);
+			}
+			else if(command == 'qa'){
+				const time = Number(args.shift());
+				message.channel.send(delimitator_a + intrebare.toString().split(delimitator_b)[0]);
+				if(time >= 10){
+					setTimeout(function(){
+						client.channels.cache.find(ch => ch.name === 'echipa-roz').send('10 secunde');
+						client.channels.cache.find(ch => ch.name === 'echipa-negru').send('10 secunde');
+						client.channels.cache.find(ch => ch.name === 'echipa-verde').send('10 secunde');
+					}, (time - 10) * 1000);
+				}
+				setTimeout(function(){
+						client.channels.cache.find(ch => ch.name === 'echipa-roz').send('Timpul');
+						client.channels.cache.find(ch => ch.name === 'echipa-negru').send('Timpul');
+						client.channels.cache.find(ch => ch.name === 'echipa-verde').send('Timpul');
+						message.channel.send(delimitator_b + intrebare.toString().split(delimitator_b)[1]);
+				}, (time) * 1000);
+			}
+		}
 	}
 })
 
